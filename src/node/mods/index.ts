52dbeb854a33e1c420982821218e4802ce4e3f72
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-22 11:58:23 +0700

    rename

--- src/node/mods/index.ts
+++ src/node/mods/index.ts
@@ -1,7 +1,7 @@
-export * from "../../wasm/pkg/ed25519_dalek_wasm.js";
+export * from "../../wasm/pkg/ed25519_wasm.js";
 
-import init from "../../wasm/pkg/ed25519_dalek_wasm.js";
-import { data } from "../../wasm/pkg/ed25519_dalek_wasm.wasm.js";
+import init from "../../wasm/pkg/ed25519_wasm.js";
+import { data } from "../../wasm/pkg/ed25519_wasm.wasm.js";
 
 export async function initBundled() {
   return await init({ module_or_path: data })
